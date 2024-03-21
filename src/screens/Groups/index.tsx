b2c9f063b5271346e4dbc4b70cb1f24e5a3472d1
Lucas Roseti <lc.roseti@gmail.com>
2024-03-20 23:05:13 -0300

    Highlight and GroupCard components

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,18 @@
 import { Header } from '@components/Header'
+import { Highlight } from '@components/Highlight'
 import { Container } from './styles'
+import { GroupCard } from '@components/GroupCard'
 
 export function Groups() {
   return (
     <Container>
       <Header />
+      <Highlight
+        title="Turmas"
+        subtitle="Jogue com a sua turma"
+      />
+
+      <GroupCard title="Galera do Ignite" />
     </Container>
   )
 }
