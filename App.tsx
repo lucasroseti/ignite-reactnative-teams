11cad75d0ae173d0fd07d4891de65612d12a18f9
Lucas Roseti <lc.roseti@gmail.com>
2024-03-20 21:50:45 -0300

    Loading component

--- App.tsx
+++ App.tsx
@@ -1,16 +1,16 @@
-import { ActivityIndicator } from 'react-native'
 import { ThemeProvider } from 'styled-components'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
 import { Groups } from '@screens/Groups'
 import theme from './src/theme'
+import { Loading } from '@components/Loading'
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
 
   return (
     <ThemeProvider theme={theme}>
-      {fontsLoaded ? <Groups /> : <ActivityIndicator /> }
+      {fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
