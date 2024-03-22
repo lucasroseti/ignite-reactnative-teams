c34175ce3c4ff1b1a9faac46b67b1ebe522ff33d
Lucas Roseti <lc.roseti@gmail.com>
2024-03-21 21:50:47 -0300

    Button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
+import { FlatList } from 'react-native'
+
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
-import { Container } from './styles'
 import { GroupCard } from '@components/GroupCard'
-import { FlatList } from 'react-native'
 import { ListEmpty } from '@components/ListEmpty'
+import { Button } from '@components/Button'
+
+import { Container } from './styles'
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([])
@@ -29,6 +32,10 @@ export function Groups() {
         ListEmptyComponent={() => (
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
+      />
+
+      <Button 
+        title='Criar nova turma'
       />
     </Container>
   )
