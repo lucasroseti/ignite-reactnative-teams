b8c29fe1785e1b3f621916d05bc8488a2329604d
Lucas Roseti <lc.roseti@gmail.com>
2024-03-21 21:35:01 -0300

    Listing groups

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Container } from './styles'
 import { GroupCard } from '@components/GroupCard'
+import { FlatList } from 'react-native'
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Galera da Rocket'])
+
   return (
     <Container>
       <Header />
@@ -12,7 +16,14 @@ export function Groups() {
         subtitle="Jogue com a sua turma"
       />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard title={item} />
+        )}
+      />
+
     </Container>
   )
 }
