35b2629e96da076ce3197797cd5d4ebf2c7bb171
Lucas Roseti <lc.roseti@gmail.com>
2024-03-23 12:15:44 -0300

    Navigation
    - React Navigation
    - Stack Navigation
    - Routes

--- App.tsx
+++ App.tsx
@@ -2,10 +2,8 @@ import { StatusBar } from 'react-native'
 import { ThemeProvider } from 'styled-components'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 
-// import { Groups } from '@screens/Groups'
-// import { NewGroup } from '@screens/NewGroup'
 import { Loading } from '@components/Loading'
-import { Players } from '@screens/Players'
+import { Routes } from 'src/routes'
 import theme from './src/theme'
 
 export default function App() {
@@ -18,7 +16,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Players /> : <Loading /> }
+      {fontsLoaded ? <Routes /> : <Loading /> }
     </ThemeProvider>
   );
 }
