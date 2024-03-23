5255609f6d1a63d5e5988c26fbefc21ad7eff90a
Lucas Roseti <lc.roseti@gmail.com>
2024-03-23 16:34:11 -0300

    Navigate to Players screen

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -31,6 +31,10 @@ export function Groups() {
     navigation.navigate('new')
   }
 
+  function handleOpenGroup(group: string) {
+    navigation.navigate('players', { group })
+  }
+
   useFocusEffect(useCallback(() => {
     fetchGroups()
   }, []))
@@ -47,7 +51,10 @@ export function Groups() {
         data={groups}
         keyExtractor={item => item}
         renderItem={({ item }) => (
-          <GroupCard title={item} />
+          <GroupCard
+            title={item}
+            onPress={() => handleOpenGroup(item)}
+          />
         )}
         contentContainerStyle={isGroupsEmpty && { flex: 1 }}
         ListEmptyComponent={() => (
