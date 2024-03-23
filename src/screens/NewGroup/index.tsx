3832903880f122525bb54571501551a1c127ebea
Lucas Roseti <lc.roseti@gmail.com>
2024-03-23 16:23:40 -0300

    Error handling

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { Alert } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
 import { Header } from '@components/Header'
@@ -7,6 +8,7 @@ import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
 
 import { groupCreate } from '@storage/group/groupCreate'
+import { AppError } from '@utils/AppError'
 
 import { Container, Content, Icon } from './styles'
 
@@ -17,10 +19,19 @@ export function NewGroup() {
 
   async function handleNew() {
     try {
+      if (group.trim().length === 0) {
+        return Alert.alert('Novo Grupo', 'Informe o nome da turma.')
+      }
+
       await groupCreate(group)
       navigation.navigate('players', { group })
     } catch (error) {
-      console.log(error)
+      if (error instanceof AppError) {
+        Alert.alert('Novo Grupo', error.message)
+      } else {
+        Alert.alert('Novo Grupo', 'Não foi possível criar um novo grupo.')
+        console.log(error)
+      }
     }
   }
 
